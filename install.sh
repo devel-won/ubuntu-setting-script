1b4d332682050d9c78bf1ddcb04361b20c7f5cda
devel-won <develwon@gmail.com>
2019-07-29 20:31:25 +0900

    edit install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
+sudo apt-get update
 
+###### install vim #############################
+
+sudo apt-get install -y vim
+
+# TODO : skel안에 있는 .vimrc 복사해서 넣기
+
+####### install git ############################
+
+sudo apt-get install -y git
+
+# change to your email & name
+git config --global user.email "develwon@gmail.com"
+git config --global user.name "devel-won"
+
+#######
