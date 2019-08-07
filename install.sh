e7e1351340fcee174044b5ec8bbe6280a3c2b293
devel-won <develwon@gmail.com>
2019-08-07 20:00:08 +0900

    docker install 내용 추가

--- install.sh
+++ install.sh
@@ -25,10 +25,8 @@ sudo apt-get install -y \
     software-properties-common 
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
-https://hiseon.me/linux/ubuntu/install-docker/ 이거 참고하기
+sudo apt-get install -y docker-ce
 
+sudo usermod -aG docker $USER
