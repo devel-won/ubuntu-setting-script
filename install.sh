dab67af015a3a4f3b26699514595f5669290830e
devel-won <develwon@gmail.com>
2019-08-07 20:10:57 +0900

    nvidia-docker 설치 추가

--- install.sh
+++ install.sh
@@ -30,3 +30,13 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get install -y docker-ce
 
 sudo usermod -aG docker $USER
+
+###### install nvidia-docker ######################
+
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
+
