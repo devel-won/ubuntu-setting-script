65a999c4db63e6ed2a0a4136818bf3a2300a6e07
devel-won <develwon@gmail.com>
2019-11-17 18:44:58 +0900

    install.sh 수정

--- install.sh
+++ install.sh
@@ -43,4 +43,21 @@ sudo systemctl restart docker
 
 sudo apt-get update && sudo apt-get install -y nvidia-docker2
 
-######
+###### install java & sbt  ###############################
+
+sudo apt-get install openjdk-8-jdk
+
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+
+curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo apt-key add
+
+sudo apt-get update
+
+sudo apt-get install sbt
+
+####### install intellij ###############################
+
+sudo cp -r pkg/idea-IC-192.7142.36 /opt
+bash /opt/idea-IC-192.7142.36/bin/idea.sh
+
+####
